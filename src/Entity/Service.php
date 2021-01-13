ca4d1ea16eecbcf591017effbe680b1fab9d93b2
leonelrm <69789534+leonelrm@users.noreply.github.com>
2021-01-13 11:45:47 +0100

    Merge remote-tracking branch 'origin/dev-3' into dev-3
    # Conflicts:
    #	.idea/php.xml
    update 5
    migration

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -3,6 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\ServiceRepository;
+use DateTime;
+use DateTimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -47,6 +51,16 @@ class Service
      */
     private $updateAt;
 
+    /**
+     * @ORM\OneToMany(targetEntity=RendezVous::class, mappedBy="Service")
+     */
+    private $RendezVous;
+
+    public function __construct()
+    {
+        $this->RendezVous = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -88,39 +102,89 @@ class Service
         return $this;
     }
 
-    public function getDureeService(): ?\DateTimeInterface
+    public function getDureeService(): ?DateTimeInterface
     {
         return $this->dureeService;
     }
 
-    public function setDureeService(\DateTimeInterface $dureeService): self
+    public function setDureeService(DateTimeInterface $dureeService): self
     {
         $this->dureeService = $dureeService;
 
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeInterface
+    public function getCreatedAt(): ?DateTimeInterface
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function setCreatedAt(DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
 
         return $this;
     }
 
-    public function getUpdateAt(): ?\DateTimeInterface
+    public function getUpdateAt(): ?DateTimeInterface
     {
         return $this->updateAt;
     }
 
-    public function setUpdateAt(?\DateTimeInterface $updateAt): self
+    public function setUpdateAt(?DateTimeInterface $updateAt): self
     {
         $this->updateAt = $updateAt;
 
         return $this;
+    }
+
+    /**
+     * @return Collection|RendezVous[]
+     */
+    public function getRendezVous(): Collection
+    {
+        return $this->RendezVous;
+    }
+
+    public function addRendezVou(RendezVous $rendezVou): self
+    {
+        if (!$this->RendezVous->contains($rendezVou)) {
+            $this->RendezVous[] = $rendezVou;
+            $rendezVou->setService($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRendezVou(RendezVous $rendezVou): self
+    {
+        if ($this->RendezVous->removeElement($rendezVou)) {
+            // set the owning side to null (unless already changed)
+            if ($rendezVou->getService() === $this) {
+                $rendezVou->setService(null);
+            }
+        }
+
+        return $this;
+    }
+    /**
+     * @ORM\PrePersist
+     */
+    public function beforePersist()
+    {
+        $this->createdAt = new DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function beforeUpdate()
+    {
+
+        $this->updateAt = new DateTime();
+    }
+    public function __toString()
+    {
+        return $this->nomService;
     }
 }
---
+++ src/Form/RendezVousType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\RendezVous;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RendezVousType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Service')
+            ->add('prixRdv')
+            ->add('dateRdv')
+            ->add('immatriculation')
+            ->add('carteGrise')
+
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => RendezVous::class,
+        ]);
+    }
+}
